2805fb41ac35ae952a8c9aab2318df8d071fa4e5
st46593 <st46593@student.upce.cz>
2020-07-23 09:47:23 +0200

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,6 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
