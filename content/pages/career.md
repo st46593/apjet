ea59a544c9c4977a2803d30bf927315640d3c941
st46593 <st46593@student.upce.cz>
2020-12-17 17:38:13 +0100

    career the end

--- content/pages/career.md
+++ content/pages/career.md
@@ -1,7 +1,7 @@
 ---
 template: CareerPage
 slug: career
-title: Kariéra
+title: Aktuálně nabízíme tyto pozice
 featuredImage: https://ucarecdn.com/e0452786-91ee-4632-9aa6-ddcd23edfe61/
 careerInfo: "V případě zájmu nám napište na email: **obchod@apjet.cz**, nebo nám
   zavolejte na telefonní číslo: **+420 734 207 427**."
--- src/templates/CareerPage.js
+++ src/templates/CareerPage.js
@@ -3,19 +3,22 @@ import { graphql } from 'gatsby'
 
 import CareerSection from '../components/CareerSection'
 import Layout from '../components/Layout'
+import Content from '../components/Content'
 import './CareerPage.css'
 
 export const CareerPageTemplate = ({
   title,
   subtitle,
   featuredImage,
+  careerInfo,
   jobOffers = []
 }) => (
         <main className="Blog">
           {jobOffers.length > 0 ? (
             <section className="section">
               <div className="container">
-                <h4 className="Career--Header">Aktuálně nabízíme tyto pozice</h4>
+                <h4 className="Career--Header">{title}</h4>
+                <Content source={careerInfo}/>
                 <CareerSection jobOffers={jobOffers} />
               </div>
             </section>
@@ -49,6 +52,7 @@ export const pageQuery = graphql`
         template
         subtitle
         featuredImage
+        careerInfo
         jobOffers {
           body
           title
